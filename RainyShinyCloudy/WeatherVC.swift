9fddd583f0932718380b725fc7619b8813239c58
Ken Krippeler <kriken19@gmail.com>
2017-07-06 23:55:39 +0200

    Setup of the UI and Outlets

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Ken Krippeler on 06.07.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController
+{
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
